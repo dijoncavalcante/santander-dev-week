330fd17a9a69dc0035222ab153d718e1c60de9a9
dijoncavalcante <dijoncavalcante@gmail.com>
2021-06-07 23:34:28 -0400

    feature: Add main layout using constraint layout. add images assets

--- app/src/main/java/com/dijon/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/dijon/santanderdevweek/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.dijon.santanderdevweek
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -12,7 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
